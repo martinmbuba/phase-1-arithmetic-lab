d27fa6179309800c700c1e857ae6e580f9215390
martinmbuba <martinmbuba4@gmail.com>
2025-06-10 21:37:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 15, 20);
